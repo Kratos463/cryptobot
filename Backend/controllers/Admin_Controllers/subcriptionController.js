e9907dfacb653bdd7c4835ff6c1eb42a5eec7294
Aleena Vincent <aleenavincent4u@gmail.com>
2024-07-06 17:23:43 +0530

    subscription deletion functionality

--- Backend/controllers/Admin_Controllers/subcriptionController.js
+++ Backend/controllers/Admin_Controllers/subcriptionController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require('express-async-handler');
-const Plan = require('../../model/planModel'); 
+const Plan = require('../../model/planModel');
 
 
 // ---------- Adding new subcription plan ---------------
@@ -8,7 +8,7 @@ const add_Plan = asyncHandler(async (req, res) => {
 
 
     const { planName, price, duration, description, features } = req.body;
-       if (!planName || !price || !duration || !description || !features) {
+    if (!planName || !price || !duration || !description || !features) {
         res.status(400);
         throw new Error('Please provide all required fields');
     }
@@ -29,11 +29,10 @@ const add_Plan = asyncHandler(async (req, res) => {
 
 // --------- Get subcription plans ------------
 
-const getPlans = asyncHandler(async (req,res)=>{
-    try {
-        
-        const plans = await Plan.find();
+const getPlans = asyncHandler(async (req, res) => {
 
+    try {
+        const plans = await Plan.find();
         res.status(200).json({ plans });
     } catch (error) {
         console.error('Error in fetching plans:', error);
@@ -41,6 +40,28 @@ const getPlans = asyncHandler(async (req,res)=>{
     }
 })
 
-module.exports = { add_Plan,
-    getPlans
+// --------Delete the subcription plan by id--------------
+
+const DeletePlan = asyncHandler(async (req, res) => {
+
+    const { planId } = req.params;
+  
+    try {
+        const deletedPlan = await Plan.findByIdAndDelete(planId);
+        if (!deletedPlan) {
+            return res.status(404).json({ message: 'Plan not found' });
+        }
+
+        res.json({ message: 'Plan deleted successfully', deletedPlan });
+    } catch (error) {
+        console.error('Error deleting plan:', error);
+        res.status(500).json({ message: 'Failed to delete plan' });
+    }
+
+})
+
+module.exports = {
+    add_Plan,
+    getPlans,
+    DeletePlan
 };
--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,10 +1,15 @@
 const {Router} = require('express');
 const {AdminLogin} = require('../controllers/Admin_Controllers/LoginController');
-const {add_Plan,getPlans}= require('../controllers/Admin_Controllers/subcriptionController');
+const {add_Plan,
+    getPlans,
+    DeletePlan}= require('../controllers/Admin_Controllers/subcriptionController');
 
 const router = Router();
 router.post('/login',AdminLogin);
+
+// -----subcription routes---------
 router.post('/add_subscription',add_Plan)
 router.get('/fetch_subcriptionPlans',getPlans)
+router.delete('/delete_plan/:planId',DeletePlan)
 
 module.exports = router
